5b64bd7d1cb87d33a60818118f3ada711a0adb56
HimanshuKabra01 <kabrahimanshu123@gmail.com>
2025-01-14 18:38:40 +0530

    Update calculator.js

--- calculator.js
+++ calculator.js
@@ -1,7 +1,12 @@
-let initial = '';
+let initial = localStorage.getItem('initial') || '';
 
-document.querySelector('.js-calcula')
-  .innerHTML = localStorage.getItem('initial');
+if (!initial) {
+  document.querySelector('.js-calcula')
+    .innerHTML = '';
+} else {
+  document.querySelector('.js-calcula')
+    .innerHTML = initial;
+}
 
 function updateCalculation(value) {
   initial += value
